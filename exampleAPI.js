b96a4f4980bd9fff22cd008fda12947b4129ca6a
alph <go_ip@mail.ru>
2018-11-13 19:14:41 +0300

    hot fix

--- exampleAPI.js
+++ exampleAPI.js
@@ -41,6 +41,7 @@ async function stakesRound(params) {
     let {players, stakes, starting_hands} = params
     let player_id = 0
     let lord_id = 0
+    let maxScore = 0
     let starting_hands2 = starting_hands.map(h => [...h])
     while (player_id < players.length) {
         let req = await POST_req(players[player_id], players[player_id].paths.score, {stakes, 
@@ -48,10 +49,14 @@ async function stakesRound(params) {
         stakes.push(req.score)
         console.log(`${players[player_id].botId} call ${req.score} with hand ${cards_to_string(players[player_id].cards).join('')}`);
         if (req.score == 3) return player_id
-        if (req.score > 0) lord_id = player_id
+        if (req.score > maxScore) {
+            maxScore = req.score
+            lord_id = player_id
+        }
         player_id++
         starting_hands2.push(starting_hands2.shift())
     }
+    if (maxScore) return lord_id
     return -1
 }
 
